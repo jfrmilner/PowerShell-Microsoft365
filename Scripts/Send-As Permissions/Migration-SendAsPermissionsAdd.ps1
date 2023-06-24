53d45f0f98cb54ff64ae4ab2b2922a7b21bfe215
jfrmilner <jfrmilner@googlemail.com>
2023-06-24 20:56:19 +0100

    Split SendAsPermissions script
    Split code into two script files.

--- Scripts/Send-As Permissions/Migration-SendAsPermissionsAdd.ps1
+++ Scripts/Send-As Permissions/Migration-SendAsPermissionsAdd.ps1
@@ -3,27 +3,21 @@ break #Until this code is wrapped into a function please edit manually and use R
 <#
 SendAs AccessRight Permission Script for use during Exchange Online Mailbox Migration transitional phase only
 This script applies a workaround of using Recipient Permissions when Mailbox Permissions are not possible, usually because the target Mailbox of the Permission entry has not been migrated.
-Once both elements of the AccessRight Permission are present in the same Exchange Org (Exchange Online/On-Prem) it is recommended that the Recipient Permission be removed and Mailbox Permission are applied. 
+Once both elements of the AccessRight Permission are present in the same Exchange Org (Exchange Online/On-Prem) it is recommended that the Recipient Permission be removed and Mailbox Permission are applied.
 
 Auth: jfrmilner
-Date: 2018-08-24
 
 Process Logic
-1. Produce Mailbox_AccessRightsFilteredWithUPN.xml via on-prem Exchange Server*
+1. Produce Mailbox_AccessRightsFilteredWithUPN.xml via on-prem Exchange Server with Migration-SendAsPermissionsAdd.ps1
 2. Import Mailbox_AccessRightsFilteredWithUPN.xml
 3. Check if Migrated User from Batch had existing permission(s)
 4. If yes then apply to Recipient Object if not already present else skip
 
-*
-##Access Rights of Extended Rights Mailbox Permissions Export (On-Prem)
-$netBIOSName = (Get-ADDomain -Current LocalComputer).NetBIOSName
-$mbExtRgts = Get-Mailbox | Get-ADPermission | ? { $_.ExtendedRights -like "*Send-As*" -and $_.IsInherited -eq $false -and $_.User.toString() -ne 'NT Authority\Self' -and $_.User -match "^$netBIOSName\\" } 
-#Add UPN of User
-$mbExtRgts | Add-Member -MemberType ScriptProperty -Name UserPrincipalName -Value { (Get-ADUser ($this.User -replace "$netBIOSName\\")).UserPrincipalName } -Force
-#Add UPN of Identity (Target)
-$mbExtRgts | Add-Member -MemberType ScriptProperty -Name IdentityUserPrincipalName -Value { (Get-Mailbox -Identity $this.Identity).UserPrincipalName } -Force
-#Export
-$mbExtRgts | Export-Clixml C:\Support\jfrmilner\Mailbox_AccessRightsFilteredWithUPN.xml
+    .NOTES
+    Auth: jfrmilner
+    Version     Date        Comment
+    1.0         2018-08     Initial Release.
+    1.1         2023-06     Split code into two script files.
 
 #>
 
@@ -35,7 +29,7 @@ if (!($mbExtRgts)) {
 }
 
 #Exchange Online Connection and Authentication
-if (!($userCredentialO365) {
+if (!($userCredentialO365)) {
     $userCredentialO365 = Get-Credential
 }
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredentialO365 -Authentication  Basic -AllowRedirection
---
+++ Scripts/Send-As Permissions/Migration-SendAsPermissionsExport.ps1
@@ -0,0 +1,15 @@
+﻿<#
+Produce Mailbox_AccessRightsFilteredWithUPN.xml via on-prem Exchange Server
+
+#>
+
+##Access Rights of Extended Rights Mailbox Permissions Export (On-Prem)
+$netBIOSName = (Get-ADDomain -Current LocalComputer).NetBIOSName
+$mbExtRgts = Get-Mailbox | Get-ADPermission | Where-Object { $_.ExtendedRights -like "*Send-As*" -and $_.IsInherited -eq $false -and $_.User.toString() -ne 'NT Authority\Self' -and $_.User -match "^$netBIOSName\\" }
+#Add UPN of User
+$mbExtRgts | Add-Member -MemberType ScriptProperty -Name UserPrincipalName -Value { (Get-ADUser ($this.User -replace "$netBIOSName\\")).UserPrincipalName } -Force
+#Add UPN of Identity (Target)
+$mbExtRgts | Add-Member -MemberType ScriptProperty -Name IdentityUserPrincipalName -Value { (Get-Mailbox -Identity $this.Identity).UserPrincipalName } -Force
+#Export
+$mbExtRgts | Export-Clixml C:\Support\jfrmilner\Mailbox_AccessRightsFilteredWithUPN.xml
+
